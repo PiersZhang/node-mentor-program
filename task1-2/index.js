3767940b2de5b1a3a8a0638044390b9780040fb9
Piers Zhang <1159380697@qq.com>
2021-10-19 09:13:01 +0800

    fix

--- task1-2/index.js
+++ task1-2/index.js
@@ -16,65 +16,65 @@ const outputPath = path.join(__dirname, '../cvs/nodejs-hw1-ex2.txt');
  * Read the content of csv file by node
  */
 const readCSV = () => {
-    fs.readFile(inputPath, (data, err) => {
-        const table = [];
-        if (err) {
-            tips.error(err);
-            return;
-        }
-        data = data.toString();
-        let rows = [];
-        rows = data.split('\r\n');
-        for (let i = 0; i < rows.length; i++) {
-            table.push(rows[i].split(','));
-        }
-    });
+  fs.readFile(inputPath, (data, err) => {
+    const table = [];
+    if (err) {
+      tips.error(err);
+      return;
+    }
+    data = data.toString();
+    let rows = [];
+    rows = data.split('\r\n');
+    for (let i = 0; i < rows.length; i++) {
+      table.push(rows[i].split(','));
+    }
+  });
 };
 
 /**
  * load all the content of the csv file into RAM via stream
  */
 const transformCSVToTxt = () => {
-    csv().fromFile(inputPath).then((json) => {
-        let txt = '';
-        Array.from(json, (item, index) => {
-            txt += JSON.stringify(item) + (json.length - 1 === index ? '' : '\r\n');
-        });
-        fs.writeFile(outputPath, txt, (err) => {
-            if (err) {
-                tips.error(err);
-                return;
-            }
-        });
-        tips.success('transform', txt);
+  csv().fromFile(inputPath).then((json) => {
+    let txt = '';
+    Array.from(json, (item, index) => {
+      txt += JSON.stringify(item) + (json.length - 1 === index ? '' : '\r\n');
     });
+    fs.writeFile(outputPath, txt, (err) => {
+      if (err) {
+        tips.error(err);
+        return;
+      }
+    });
+    tips.success('transform', txt);
+  });
 };
 
 /**
  * load the content of the csv file line by line
  */
 const transformCSVToTxtByline = () => {
-    const input = fs.createReadStream(inputPath);
-    const output = fs.createWriteStream(outputPath);
-    const rl = readline.createInterface({
-        input,
-        output
+  const input = fs.createReadStream(inputPath);
+  const output = fs.createWriteStream(outputPath);
+  const rl = readline.createInterface({
+    input,
+    output
+  });
+  const arr = [];
+  rl.on('line', (line) => {
+    arr.push(line.split(','));
+  });
+  rl.on('close', () => {
+    const [titleArr, ...contentArr] = arr;
+    contentArr.map((item, num) => {
+      const book = {};
+      titleArr.map((obj, index) => {
+        book[obj] = item[index];
+      });
+      output.write(JSON.stringify(book) + (contentArr.length <= num + 1 ? '' : '\r\n'));
+      return book;
     });
-    const arr = [];
-    rl.on('line', (line) => {
-        arr.push(line.split(','));
-    });
-    rl.on('close', () => {
-        const [titleArr, ...contentArr] = arr;
-        contentArr.map((item, num) => {
-            const book = {};
-            titleArr.map((obj, index) => {
-                book[obj] = item[index];
-            });
-            output.write(JSON.stringify(book) + (contentArr.length <= num + 1 ? '' : '\r\n'));
-            return book;
-        });
-    });
+  });
 };
 
 
