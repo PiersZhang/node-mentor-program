4c0e4b46c5db6fb887c60fedee661cb0c411127b
unknown <zhangpiers@gmail.com>
2021-12-04 19:07:48 +0800

    fix

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -3,7 +3,7 @@ import { HttpException } from '../http-exception/index';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function errorMiddleware(error: HttpException, request: Request, response: Response, next: NextFunction): void {
-  const status = error.status;
+  const status = error.status || 500;
   const message = error.message;
   return response.status(status).send({
     status,
