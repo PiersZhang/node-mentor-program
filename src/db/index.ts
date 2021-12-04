cadc6ff31862aa7fe06cc6b283a25bc0ecbac03f
unknown <zhangpiers@gmail.com>
2021-12-04 19:24:34 +0800

    add

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,7 @@ import { Sequelize } from 'sequelize-typescript';
 const sequelize = new Sequelize('piersdb', 'postgres', '123123', {
   host: 'localhost',
   dialect: 'postgres',
-  models:  [`${__dirname  }/src/models`]
+  models:  [`${__dirname}/src/entity`]
 });
-
 
 export default sequelize;
